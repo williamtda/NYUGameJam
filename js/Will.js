8bc230bd79183731d734c31b7dff1d5dd65cae85
willstreet <william.alexander13@stjohns.edu>
2016-12-10 18:36:58 -0500

    t

--- js/Will.js
+++ js/Will.js
@@ -28,7 +28,7 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
-    var sky = game.add.sprite(0, 0, 'sky');
+    //var sky = game.add.sprite(0, 0, 'sky');
 	var school = game.add.tileSprite(0, 0, 'school')
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
