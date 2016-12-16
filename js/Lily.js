ff6dec20f24bd121c7439f4bc691ac25531d1207
lilyz622 <lily.zhang622@yahoo.com>
2016-12-16 16:26:13 -0500

    lily

--- js/Lily.js
+++ js/Lily.js
@@ -7,7 +7,7 @@ WebFontConfig = {
     //  'active' means all requested fonts have finished loading
     //  We set a 1 second delay before calling 'createText'.
     //  For some reason if we don't the browser cannot render the text the first time it's created.
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, , this); },
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, empty, this); },
 
     //  The Google Fonts we want to load (specify as many as you like in the array)
     google: {
@@ -16,6 +16,7 @@ WebFontConfig = {
 
 };
 
+function empty(){}
 
 function preload() {
 
