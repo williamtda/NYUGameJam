845b393d1b026c9c72781640ea22068a9f102b08
willstreet <william.alexander13@stjohns.edu>
2016-12-16 21:37:20 -0500

    multiple timer test

--- js/Will.js
+++ js/Will.js
@@ -99,7 +99,7 @@ function create() {
 	
 	//The Time left
 	timerText = game.add.text(600, 16, 'Time left: 33', { fontSize: '32px', fill: '#000' });
-	game.time.events.loop(Phaser.Timer.SECOND, updateTimer, this);
+	game.time.events.loop(1000, updateTimer, this);
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
@@ -111,20 +111,20 @@ function create() {
 
     //  Once the event has been called 2 times it will never be called again.
 
-    game.time.events.repeat(Phaser.Timer.SECOND * 5, 2, createHomework, this);
+    game.time.events.repeat(1000 * 5, 2, createHomework, this);
 	
 	//  AT 15 SECOND MARK
 	//  Here we'll create a basic timed event. This is a one-off event, it won't repeat or loop:
     //  The first parameter is how long to wait before the event fires. In this case 15 seconds 
     //  The next parameter is the function to call ('halfTime') and finally the context under which that will happen.
 
-    game.time.events.add(Phaser.Timer.SECOND * 15, halfTime, this);
+    game.time.events.add(1000 * 15, halfTime, this);
 	
 	//  AT 30 SECONDS
-	game.time.events.add(Phaser.Timer.SECOND * 30, createTest, this);
+	game.time.events.add(1000 * 30, createTest, this);
 	
 	//  AT 33 SECONDS?
-	game.time.events.add(Phaser.Timer.SECOND * 33, endGame, this); // Testing purposes only
+	game.time.events.add(1000 * 33, endGame, this); // Testing purposes only
 }
 
 function returnGrade(score){
@@ -305,7 +305,7 @@ function createTest() {
 function halfTime(homework){
 	
 	createTest();
-	game.time.events.repeat(Phaser.Timer.SECOND * 5, 2, createHomework, this);
+	game.time.events.repeat(1000 * 5, 2, createHomework, this);
 }
 
 function render() {
